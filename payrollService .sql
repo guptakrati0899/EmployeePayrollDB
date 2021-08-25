82afffec0ad61e2a611d60296b32eed32efac2f9
KratiGupta <kratigupta8545@gmail.com>
2021-08-25 23:18:23 +0530

    Added insertion of details

--- payrollService .sql
+++ payrollService .sql
@@ -6,6 +6,12 @@ CREATE DATABASE payroll_service;
      id         INT unsigned NOT NULL AUTO_INCREMENT,
      name       VARCHAR(150) NOT NULL,
      salary     DOUBLE NOT NULL,
-     1start      DATE NOT NULL,
+     start      DATE NOT NULL,
      PRIMARY KEY      (id)
      );
+     
+INSERT INTO employee_payroll (name, salary, start) VALUES
+ ('Krati', 400000.0, '2021-02-08'),
+('Nishu', 500000.0, '2021-03-08'),
+('Shreya', 600000.0, '2021-04-08');
+SELECT * FROM address_book;
