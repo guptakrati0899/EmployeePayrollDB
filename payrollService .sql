de25e08db99a1159db6298da9ccd93bce9489ed8
KratiGupta <kratigupta8545@gmail.com>
2021-08-25 23:50:20 +0530

    Added to extend table in order to store more info

--- payrollService .sql
+++ payrollService .sql
@@ -15,7 +15,7 @@ INSERT INTO employee_payroll (name, salary, start) VALUES
 ('Nishu', 500000.0, '2021-03-08'),
 ('Shreya', 600000.0, '2021-04-08');
 
-SELECT * FROM address_book;
+SELECT * FROM employee_payroll;
 
 SELECT salary FROM employee_payroll WHERE name = 'Krati';
 Select * FROM employee_payroll
@@ -29,3 +29,21 @@ SELECT AVG(salary) FROM employee_payroll WHERE gender = 'female' GROUP BY gender
 SELECT gender , COUNT(name) FROM employee_payroll GROUP BY gender;
  SELECT gender , SUM(name) FROM employee_payroll GROUP BY gender;
 SELECT gender, SUM(salary) FROM employee_payroll GROUP BY gender;
+
+
+ALTER TABLE employee_payroll ADD phone VARCHAR(255) AFTER name;
+ALTER TABLE employee_payroll ADD address VARCHAR(255) AFTER phone;
+ALTER TABLE employee_payroll ADD department VARCHAR(255) AFTER address;
+describe employee_payroll;
+select * from employee_payroll;
+
+UPDATE employee_payroll set phone = '8787667879' WHERE id = 2;
+UPDATE employee_payroll set phone = '9877656545' WHERE id = 1;
+UPDATE employee_payroll set phone = '7876566656' WHERE id = 3;
+UPDATE employee_payroll set address = 'Ghaziabad' WHERE id = 1;
+UPDATE employee_payroll set address = 'Kanpur' WHERE id = 2;
+UPDATE employee_payroll set address = 'Laxmi Nagar' WHERE id = 3;
+UPDATE employee_payroll set department = 'Content' WHERE id = 1;
+UPDATE employee_payroll set department = 'Content' WHERE id = 2;
+UPDATE employee_payroll set department = 'Technology' WHERE id = 3;
+select * from employee_payroll;
