0143e180499da577630bca3ed2ce0608a4ebbc88
KratiGupta <kratigupta8545@gmail.com>
2021-08-25 23:05:24 +0530

    nadded creation of table

--- payrollService .sql
+++ payrollService .sql
@@ -1,3 +1,11 @@
 CREATE DATABASE payroll_service;
  show databases;
  USE payroll_service;
+
+ CREATE TABLE employee_payroll(
+     id         INT unsigned NOT NULL AUTO_INCREMENT,
+     name       VARCHAR(150) NOT NULL,
+     salary     DOUBLE NOT NULL,
+     1start      DATE NOT NULL,
+     PRIMARY KEY      (id)
+     );
